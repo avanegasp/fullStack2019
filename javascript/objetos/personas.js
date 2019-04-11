c47d9c601c37765513214ef5f2754c7a4fd39928
Angie <arco527@gmail.com>
2019-04-10 20:51:07 -0500

    Call

--- javascript/objetos/personas.js
+++ javascript/objetos/personas.js
@@ -3,34 +3,46 @@
 */
 
 function Constructor(nombre, apellido){
+
   persona = {
     nombre: nombre,
     apellido: apellido,
     sayHi:function(){
       console.log(this)
       console.log(`Hola, soy ${this.nombre} ${this.apellido}`)
-    }
+    },
+    __proto__: Constructor.prototype
   }
   return persona
 }
 
+console.log("----------------")
+
 cindy = Constructor("Cindy", "Duarte")
+Constructor.prototype.blah = function(){
+  console.log("Blahhhh")
+}
+console.log(cindy)
 cindy.sayHi()
-
+cindy.blah()
 julian = Constructor("Julian", "Hurtado")
-julian.sayHi()
 
+julian.sayHi()
 
 constructor2 = function(nombre, apellido){
   this.nombre = nombre
   this.apellido = apellido
+  this.sayHi = function(){
+    console.log(this)
+    console.log(`Hola, soy ${this.nombre} ${this.apellido}`)
+  }
 }
 
-constructor2.sayHi =function(){
-  console.log(this)
-  console.log(`Hola, soy ${this.nombre} ${this.apellido}`)
-}
-
-oscar = new constructor2("Oscar", "Beltran")
+oscar = {nombre:"Oscar", apellido:"Beltran"}
 console.log(oscar)
-oscar.sayHi()
+//oscar.sayHi()
+
+andres = new constructor2("Andres", "Torrijos")
+
+andres.sayHi()
+andres.sayHi.call(oscar)
